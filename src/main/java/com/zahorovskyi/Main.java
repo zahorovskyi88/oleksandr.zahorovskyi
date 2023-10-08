6be6c7b36ddcfb01e6029cd78ab2632a7768b23d
Oleksandr Zahorovskyi <ezorisplace@gmail.com>
2023-10-08 19:20:40 +0300

    fixup! checking for arguments in Main.java

--- src/main/java/com/zahorovskyi/Main.java
+++ src/main/java/com/zahorovskyi/Main.java
@@ -6,7 +6,7 @@ public class Main {
         if (args.length == 3) {
             String command = args[0];
             String filePath = args[1];
-            String key = args[2];
+            int key = Integer.parseInt(args[2]);
         } else {
             throw new IllegalAccessException("Args must have 3 commands");
         }
