81d173cc9d1945846f6b7a922284c13be726fa64
Oleksandr Zahorovskyi <ezorisplace@gmail.com>
2023-10-08 18:32:01 +0300

    checking for arguments in Main.java

--- src/main/java/com/zahorovskyi/Main.java
+++ src/main/java/com/zahorovskyi/Main.java
@@ -1,7 +1,14 @@
 package com.zahorovskyi;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IllegalAccessException {
+        // java -jar myApp.jar command filePath key
+        if (args.length == 3) {
+            String command = args[0];
+            String filePath = args[1];
+            String key = args[2];
+        } else {
+            throw new IllegalAccessException("Args must have 3 commands");
+        }
     }
 }
